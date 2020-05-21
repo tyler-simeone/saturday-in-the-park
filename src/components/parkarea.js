9acfc26e4206339a30d3e22f8aeb432c75be8489
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-21 09:34:41 -0500

    removed sublist attempt in JSX from parkarea.js component

--- src/components/parkarea.js
+++ src/components/parkarea.js
@@ -18,13 +18,7 @@ const ParkArea = props => {
             <section>
                 <ul>
                 {parkareas.map(park =>
-                    <>
                     <p key={park.id}>{park.name}</p>
-                        {/* <p>{park.attractions.forEach(attraction => (
-                            // console.log(attraction.name)
-                            <p key={attraction.id}>{attraction.name}</p>
-                        ))}</p> */}
-                    </>
                 )}
                 </ul>
             </section>
